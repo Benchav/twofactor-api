d325f59249ef28c10446478de45b91dce814e7b3
Benchav <joshua44benja@gmail.com>
2026-06-27 09:48:24 -0600

    chore: remove unused test script for authentication endpoint

--- test.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:3001/api/auth/login', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ email: 'test@example.com', password: '123' })
-}).then(res => res.json()).then(console.log).catch(console.error);
